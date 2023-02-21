9d093fb42d8e160476f2c79461a7da0b5127f485
Felipe Schappo <felipeschappoo@gmail.com>
2023-02-21 01:52:34 -0300

    fix: msg-commit-linter

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,8 @@
+import { describe, expect, test } from 'vitest'
+import App from '../App'
+
+describe('Accordion test', () => {
+  test('app child components should be defined', () => {
+    expect(App).toBeDefined()
+  })
+})
